ce54528b2bbc6859edf8d880428fcd321588863e
dnjoe96 <donjoedbest@gmail.com>
2022-09-03 07:39:31 +0100

    Create view functions and `blueprint

---
+++ flaskapp/api/v0/user/models/db.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" User Schema Module"""
+from flask_sqlalchemy import Model
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class User(Base):
+    """ User class """
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
+
+    def __repr__(self):
+        """ Class object representation """
+        return '<User(id=\'%s\', email=\'%s\')>' % (
+            self.id, self.email
+        )
