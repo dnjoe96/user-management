e6df3d57ea9a875a2ff961bd6c2a1331cbb07699
dnjoe96 <donjoedbest@gmail.com>
2022-09-07 16:48:21 +0100

    Create Models and defined some views

--- flaskapp/api/v0/app.py
+++ flaskapp/api/v0/app.py
@@ -5,22 +5,31 @@ Route module for the API
 from os import getenv
 from typing import Tuple
 
-# from api.v0.user.views import app_views
 from flask import Flask, jsonify, abort, request, Response
 from flask_sqlalchemy import SQLAlchemy
 import flask_cors
+from flask_migrate import Migrate
 import os
-from api.v0 import app_views
-from api.v0.error import app_err
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
-app.register_blueprint(app_err)
-db = SQLAlchemy()
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Postgres12@soft-dev.cixnnif6iavq.us-east-1.rds.amazonaws.com/softdev01'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 flask_cors.CORS(app, resources={r"/api/v0/*": {"origins": "*"}})
 auth = os.environ.get('AUTH_TYPE', None)
+
+
+from api.v0.errors import app_err
+app.register_blueprint(app_err)
+
+from api.v0 import app_views
+# from api.v0.user import user_app
+app.register_blueprint(app_views)
 
 # if auth:
 #     if auth == 'basic_auth':
@@ -47,6 +56,11 @@ def just_before_request():
 @app.route('/')
 def index():
     return jsonify({'message': 'welcome to the root of this API'})
+
+
+@app.route('/api/v0')
+def v0_index():
+    return jsonify({'message': 'welcome to /api/v0 root API'})
 
 
 if __name__ == "__main__":
