7e25cbb05ec89092bcb3b48e68a956a945b49abd
dnjoe96 <donjoedbest@gmail.com>
2022-09-08 17:26:08 +0100

    Create View to register users and activate users

--- nodeapp/src/controllers/v0/users/models/User.js
+++ nodeapp/src/controllers/v0/users/models/User.js
@@ -1,26 +1,136 @@
-import { Table, Column, Model, HasMany, PrimaryKey, CreatedAt, UpdatedAt } from 'sequelize';
+import { compare, hash } from 'bcrypt';
+import { DataTypes, Model } from 'sequelize';
 
-@Table
-export class User extends Model<User> {
-  
-  @PrimaryKey
-  @Column
-  public email!: string;
+import { tokenHelper, mailHelper } from '@/helpers';
 
-  @Column
-  public password_hash!: string; // for nullable fields
+export default function (sequelize) {
+  class User extends Model {
+    get fullName() {
+      return `${this.firstname} ${this.lastname}`;
+    }
 
-  @Column
-  @CreatedAt
-  public createdAt: Date = new Date();
+    generateToken(expiresIn = '1h') {
+      const data = { id: this.id, email: this.email };
+      return tokenHelper.generateToken(data, expiresIn);
+    }
 
-  @Column
-  @UpdatedAt
-  public updatedAt: Date = new Date();
+    // validatePassword(plainPassword) {
+    //   return compare(plainPassword, this.password);
+    // }
 
-  short() {
-    return {
-      email: this.email
+    // sendMail(mail) {
+    //   const payload = { ...mail, to: `${this.fullName} <${this.email}>` };
+    //   return mailHelper.sendMail(payload);
+    // }
+
+    static associate(models) {
+      User.hasMany(models.tweet, { foreignKey: 'userId' });
     }
   }
+
+  User.init({
+     id: {
+     type: DataTypes.STRING,
+     allowNull: false,
+     primaryKey: true
+   },
+   username: {
+     type: DataTypes.STRING,
+     allowNull: false,
+     unique: true
+   },
+   firstname: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   lastname: {
+     type: DataTypes.STRING,
+     allowNull: false
+   },
+   middlename: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   email: {
+     type: DataTypes.STRING,
+     allowNull: false,
+     unique: true
+   },
+   phone: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   city: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   state: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   country: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   group: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   active: {
+     type: DataTypes.STRING,
+     allowNull: no,
+     defaultValue: 0
+   },
+   rank: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+   date_created: {
+     type: DataTypes.DATE,
+     allowNull: false,
+     defaultValue: DataTypes.NOW
+   },
+   date_updated: {
+     type: DataTypes.DATEO,
+     allowNull: true
+   },
+   activation_token: {
+     type: DataTypes.INTEGER,
+     allowNull: true
+   },
+   deletion_flag: {
+     type: DataTypes.STRING,
+     allowNull: true
+   },
+  }, {
+    modelName: 'user',
+    sequelize,
+  });
+
+  User.addHook('beforeSave', async (instance) => {
+    if (instance.changed('password')) {
+      // eslint-disable-next-line no-param-reassign
+      instance.password = await hash(instance.password, 10);
+    }
+  });
+
+  User.addHook('afterCreate', (instance) => {
+    // Send welcome message to user.
+    const payload = {
+      subject: 'Welcome to Express Starter',
+      html: 'Your account is created successfully!',
+    };
+    instance.sendMail(payload);
+  });
+
+  User.addHook('afterDestroy', (instance) => {
+    // Send good by message to user.
+    const payload = {
+      subject: 'Sorry to see you go',
+      html: 'Your account is destroyed successfully!',
+    };
+    instance.sendMail(payload);
+  });
+
+  return User;
 }
--- nodeapp/src/controllers/v0/users/routes/auth.router.js
+++ nodeapp/src/controllers/v0/users/routes/auth.router.js
@@ -1,26 +1,28 @@
-import { Router, Request, Response } from 'express';
-
-// import { User } from '../models/User.js';
-
 import * as bcrypt from 'bcrypt';
 import * as jwt from 'jsonwebtoken';
 
-import * as EmailValidator from 'email-validator';
 import { config } from '../../../../config/config.js';
 
-const router = Router();
 
-async function generatePassword(plainTextPassword) {
-    //@TODO Use Bcrypt to Generated Salted Hashed Passwords
-    return "NotYetImplemented"
+export async function generatePassword(plainTextPassword) {
+  //@TODO Use Bcrypt to Generated Salted Hashed Passwords
+  const salt = await bcrypt.genSalt(10);
+  // now we set user password to hashed password
+  const password = await bcrypt.hash(plainTextPassword, salt);
+  return password
 }
 
-async function comparePasswords(plainTextPassword, hash) {
-    //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
-    return true
+export async function comparePasswords(plainTextPassword, hash) {
+  //@TODO Use Bcrypt to Compare your password to your Salted Hashed Password
+  const validPassword = await bcrypt.compare(plainTextPassword, hash);
+    if (validPassword) {
+      return true
+    } else {
+      return false
+    }
 }
 
-function generateJWT(user) {
+export function generateJWT(user) {
     //@TODO Use jwt to create a new JWT Payload containing
     return jwt.sign(user.toJSON(), config.jwt.secret);
 }
@@ -47,88 +49,3 @@ export function requireAuth(req, res, next) {
     //   return next();
     // });
 }
-
-router.get('/verification', 
-    requireAuth, 
-    async (req, res) => {
-        return res.status(200).send({ auth: true, message: 'Authenticated.' });
-});
-
-router.post('/login', async (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    // check email is valid
-    if (!email || !EmailValidator.validate(email)) {
-        return res.status(400).send({ auth: false, message: 'Email is required or malformed' });
-    }
-
-    // check email password valid
-    if (!password) {
-        return res.status(400).send({ auth: false, message: 'Password is required' });
-    }
-
-    const user = await User.findByPk(email);
-    // check that user exists
-    if(!user) {
-        return res.status(401).send({ auth: false, message: 'Unauthorized' });
-    }
-
-    // check that the password matches
-    const authValid = await comparePasswords(password, user.password_hash)
-
-    if(!authValid) {
-        return res.status(401).send({ auth: false, message: 'Unauthorized' });
-    }
-
-    // Generate JWT
-    const jwt = generateJWT(user);
-
-    res.status(200).send({ auth: true, token: jwt, user: user.short()});
-});
-
-//register a new user
-router.post('/', async (req, res) => {
-    const email = req.body.email;
-    const plainTextPassword = req.body.password;
-    // check email is valid
-    if (!email || !EmailValidator.validate(email)) {
-        return res.status(400).send({ auth: false, message: 'Email is required or malformed' });
-    }
-
-    // check email password valid
-    if (!plainTextPassword) {
-        return res.status(400).send({ auth: false, message: 'Password is required' });
-    }
-
-    // find the user
-    const user = await User.findByPk(email);
-    // check that user doesnt exists
-    if(user) {
-        return res.status(422).send({ auth: false, message: 'User may already exist' });
-    }
-
-    const password_hash = await generatePassword(plainTextPassword);
-
-    const newUser = await new User({
-        email: email,
-        password_hash: password_hash
-    });
-
-    let savedUser;
-    try {
-        savedUser = await newUser.save();
-    } catch (e) {
-        throw e;
-    }
-
-    // Generate JWT
-    const jwt = generateJWT(savedUser);
-
-    res.status(201).send({token: jwt, user: savedUser.short()});
-});
-
-router.get('/', async (req, res) => {
-    res.send('auth')
-});
-
-export const AuthRouter = router;
